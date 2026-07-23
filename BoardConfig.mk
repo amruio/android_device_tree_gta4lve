95e77d2531be90a1ac404ba61f40eecb691ea721
amruio <uioamr65@gmail.com>
2026-07-24 01:33:51 +0300

    gta4lve: set BUILD_BROKEN_DUP_RULES := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,5 +88,7 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+BUILD_BROKEN_DUP_RULES := true
+
 # Inherit the proprietary files
 include vendor/samsung/gta4lve/BoardConfigVendor.mk
